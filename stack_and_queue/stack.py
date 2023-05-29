3aa960045bf7d2f8e054645307d08925a46bf100
AsemAttallah <attallahasem@gmail.com>
2023-05-29 19:59:10 +0000

    01

--- stack_and_queue/stack.py
+++ stack_and_queue/stack.py
@@ -73,19 +73,69 @@ class Stack:
             current=current.next
         return string+"None"
     
+class PseudoQueue:
+    def __init__(self,top=None):
+        self.stack = Stack(top)
+        self.stack_2 = Stack(top)
+
+    def enqueue(self, value):
+        self.stack.push(value)
+    
+    
+    def dequeue(self,value):
+        if self.stack.top==None:
+            raise Exception ("The pseudo queue is empty")
+        else:
+            while self.stack.top!=None:
+                value = self.stack.pop()
+                self.stack_2.push(value)
+            dequeued_value = self.stack_2.pop()
+
+            while self.stack_2.top!=None:
+                value = self.stack_2.pop()
+                self.stack.push(value)
+            
+            return dequeued_value
+        
+    def __str__(self):
+        '''
+        This method to return all node in stack
+        '''
+        current=self.stack.top
+        string=""
+        while current:
+            string+=f"{current.value}"
+            string+=" -> "
+            current=current.next
+        return string+"None"
 
 
 
 if __name__=="__main__":
-    stack_01=Stack()
-    stack_01.push(1)
-    stack_01.push(2)
-    stack_01.push(3)
-    print(stack_01)
+    # stack_01=Stack()
+    # stack_02=Stack()
+    pseudo_queue=PseudoQueue()
+    pseudo_queue2=PseudoQueue()
+
+    # pseudo_queue.push("asem")
+    pseudo_queue.enqueue("omar")
+    pseudo_queue.enqueue("Ahmad")
+    pseudo_queue.enqueue("essa")
+    # pseudo_queue2.enqueue("Ahmad")
+    # pseudo_queue2.enqueue("essa")
+    # stack_01.push(2)
+    # stack_01.push(3)
+    # stack_02.push("hfg")
+    # stack_02.push("ewr")
+    # pseudo_queue.push(2)
+    # print(stack_01.top)
+    print(pseudo_queue)
+    print(pseudo_queue.dequeue("essa"))
     # stack_01.push(4)
     # print(stack_01)
+    # print(stack_02)
     # print(stack_01.pop())
-    print(stack_01)
+    # print(stack_01)
     # print(stack_01.is_empty())
     # print(stack_01.peek())
     # print(stack_01.top.value)
