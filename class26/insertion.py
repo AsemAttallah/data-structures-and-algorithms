d5fe3971310ddcd43a50bd25fe11eb57f0a5d37f
AsemAttallah <attallahasem@gmail.com>
2023-07-11 06:47:59 +0000

    26

--- class26/insertion.py
+++ class26/insertion.py
@@ -5,9 +5,11 @@ nearly_sorted = [2,3,5,7,13,11]
 def insertion(arr):
     sorted_arr=[arr[0]]
     for i in range(len(arr)-1):
-        if arr[i+1]>sorted_arr[i]:
+        if arr[i+1]>=sorted_arr[i]:
             sorted_arr.append(arr[i+1])
-        elif arr[i+1]<sorted_arr[i]:
+        elif arr[i+1]<=sorted_arr[i] and arr[i+1]>=sorted_arr[i-1]:
+            sorted_arr.insert(i,arr[i+1])
+        elif arr[i+1]<=sorted_arr[i]:
             sorted_arr.insert(0,arr[i+1])
     return sorted_arr
         
